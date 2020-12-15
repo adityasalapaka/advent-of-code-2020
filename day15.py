22904cacf3fd7b12688f3aeab5bcc119a3885421
Aditya Salapaka <salapaka.aditya@gmail.com>
2020-12-15 10:20:30 -0500

    solved day15

---
+++ day15.py
@@ -0,0 +1,43 @@
+file = [9,12,1,4,17,0,18]
+
+from collections import defaultdict
+from tqdm import tqdm
+
+def compute_last_spoken(memory:list, nth_number_spoken:int) -> int:
+	memory = file.copy()
+	spoken = defaultdict(list)
+
+	for i in range(len(memory)):
+		
+		spoken[memory[i]].append(i)
+		last = memory[i]
+
+	i +=1 
+
+	# progress bar to check solution progress
+	pbar = tqdm(total = nth_number_spoken, initial = i)
+
+	while i < nth_number_spoken:
+
+		last_spoken_times = len(spoken[last])
+
+		if last_spoken_times == 1:
+			spoken[0].append(i)
+			last = 0
+		else:
+			indices = spoken[last]
+			last = indices[-1] - indices[-2]
+			spoken[last].append(i)
+
+		i+=1
+		pbar.update(1)
+
+	pbar.close()
+
+	return last
+
+# part 1
+print(compute_last_spoken(file, 2020))
+
+# part 2
+print(compute_last_spoken(file, 30000000))
